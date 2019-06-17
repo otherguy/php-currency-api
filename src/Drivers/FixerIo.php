7a60ccb19013fa44c828f789795b926e653915f0
Alexander Graf <alex@basecamp.tirol>
2019-06-17 12:45:29 +0200

    Update FixerIo.php

--- src/Drivers/FixerIo.php
+++ src/Drivers/FixerIo.php
@@ -138,7 +138,7 @@ class FixerIo extends BaseCurrencyDriver implements CurrencyDriverContract
     // Handle response exceptions.
     if ($response['success'] == false) {
       $message = "[{$response['error']['type']}]";
-      if (isset($response['error']['info']) {
+      if (isset($response['error']['info'])) {
         $message .= ' ' . $response['error']['info'];
       }
       throw new ApiException($message, $response['error']['code']);
